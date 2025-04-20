1080622ea1e468f29dc7011ae50e798bfca7ba5f
Rifat_Sayeed <rifatahmed21095@gmail.com>
2025-04-21 02:22:49 +0600

    Update deleteBSTlab20.cpp

--- deleteBSTlab20.cpp
+++ deleteBSTlab20.cpp
@@ -1,131 +1,87 @@
-#include <iostream>
-#include <sstream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+struct node
 {
     int data;
-    Node *left, *right;
-    Node(int val)
+    node *left, *right;
+    node(int val)
     {
         data = val;
-        left = right = nullptr;
+        left = right = NULL;
     }
 };
 
-// Insert function for Binary Search Tree
-Node *insertBst(Node *root, int val)
+node *insert(node *root, int data)
 {
-    if (root == nullptr)
-    {
-        return new Node(val);
-    }
-    if (val < root->data)
-    {
-        root->left = insertBst(root->left, val);
-    }
-    else if (val > root->data)
-    {
-        root->right = insertBst(root->right, val);
-    }
+    if (root == NULL)
+        return new node(data);
+    else if (data > root->data)
+        root->right = insert(root->right, data);
+    else if (data < root->data)
+        root->left = insert(root->left, data);
     return root;
 }
 
-// Find the inorder successor (smallest value in the right subtree)
-Node *minValueNode(Node *root)
+bool search(node *root, int val)
 {
-    Node *temp = root;
-    while (temp->left != nullptr)
+    if (root == NULL)
+        return false;
+    else if (root->data == val)
+    {
+        return true;
+        
+    }
+
+    else if (val > root->data)
+        return search(root->right, val);
+    else
+        return search(root->left, val);
+}
+node *minValue(node *root)
+{
+    node *temp = root;
+    while (temp->left != NULL)
     {
         temp = temp->left;
     }
     return temp;
 }
-
-// Delete function for Binary Search Tree
-Node *deleteBst(Node *root, int val)
+node *deleteb(node *root, int data)
 {
-    // base case
-    if (root == nullptr)
-    {
+    if (root == NULL)
         return root;
-    }
-    if (val < root->data)
+    else if (data > root->data)
+        root->right = deleteb(root->right, data);
+    else if (data < root->data)
+        root->left = deleteb(root->left, data);
+    else
     {
-        root->left = deleteBst(root->left, val);
-    }
-    else if (val > root->data)
-    {
-        root->right = deleteBst(root->right, val);
-    }
-    else //(root data==val)
-    {
-        // 0 child
         if (root->left == NULL && root->right == NULL)
         {
             delete root;
             return NULL;
         }
-        // 1 child
-
-        // right child
-        if (root->left == nullptr && root->right != NULL)
+        if (!root->left)
         {
-            Node *temp = root->right;
+            node *temp = root->right;
             delete root;
             return temp;
         }
-
-        // left child
-        if (root->right == nullptr && root->left != NULL)
+        if (!root->right)
         {
-            Node *temp = root->left;
+            node *temp = root->left;
             delete root;
             return temp;
         }
-
-        // 2 child
-        if (root->left != NULL && root->right != NULL)
-
-        {
-            int mini = minValueNode(root->right)->data;
-            root->data = mini;
-            root->right = deleteBst(root->right, mini);
-        }
+        int mini = minValue(root->right)->data;
+        root->data = mini;
+        root->right = deleteb(root->right, mini);
     }
     return root;
 }
 
-// Search function for Binary Search Tree
-bool searchBst(Node *root, int val)
-{
-    if (root == nullptr)
-    {
-        return false;
-    }
-    if (root->data == val)
-    {
-        return true;
-    }
-    if (val < root->data)
-    {
-        return searchBst(root->left, val);
-    }
-    return searchBst(root->right, val);
-}
-
-// Traversals
-void inorder(Node *root)
-{
-    if (root)
-    {
-        inorder(root->left);
-        cout << root->data << " ";
-        inorder(root->right);
-    }
-}
-void preorder(Node *root)
+void preorder(node *root)
 {
     if (root)
     {
@@ -134,76 +90,93 @@ void preorder(Node *root)
         preorder(root->right);
     }
 }
-void postorder(Node *root)
+void postorder(node *root)
 {
     if (root)
     {
+
         postorder(root->left);
         postorder(root->right);
         cout << root->data << " ";
     }
 }
 
-void displayTraversals(Node *root)
+void inorder(node *root)
 {
-    cout << "Inorder Traversal: ";
-    inorder(root);
-    cout << "\nPreorder Traversal: ";
-    preorder(root);
-    cout << "\nPostorder Traversal: ";
-    postorder(root);
-    cout << endl;
+    if (root)
+    {
+
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
 }
 
-void clearScreen()
+void display(node *root)
 {
-    system("cls"); // For Windows, use "clear" for Linux/Mac
+    cout << "inorder:";
+    inorder(root);
+    cout << "\npreorder:";
+    preorder(root);
+    cout << "\npostorder:";
+    postorder(root);
+    cout << "\n";
 }
 
 int main()
 {
-    Node *root = nullptr;
-    cout << "Enter tree elements separated by space (type -1 to stop): ";
-
-    int num;
-    while (cin >> num && num != -1)
-    { // Read input until -1 is entered
-        root = insertBst(root, num);
-    }
+    node *root = NULL;
 
     int choice;
-    do
+
+    while (true)
     {
-        cout << "\nMenu:\n1. Display Traversals\n2. Search an Element\n3. Delete an Element\n4. Exit\nEnter your choice: ";
+        cout << "---------MENU-----------\n";
+        cout << "1.input\n";
+        cout << "2.search\n";
+        cout << "3.delete\n";
+        cout << "4.display\n";
+        cout<<"5.exit\n";
+        cout<<"Enter your choice: ";
         cin >> choice;
-        clearScreen();
 
         switch (choice)
         {
         case 1:
-            displayTraversals(root);
+            cout << "Enter tree elements: ";
+            int num;
+
+            while (cin >> num && num != -1)
+            {
+                root = insert(root, num);
+            }
+
             break;
         case 2:
-            int searchValue;
-            cout << "Enter value to search: ";
-            cin >> searchValue;
-            cout << searchValue << (searchBst(root, searchValue) ? " found" : " not found") << " in BST.\n";
+            int data;
+            cin >> data;
+            search(root, data);
+            if (search(root, data))
+                cout << "Data found in BST\n";
+            else
+                cout << "Data not found in BST\n";
             break;
         case 3:
-            int deleteValue;
-            cout << "Enter value to delete: ";
-            cin >> deleteValue;
-            root = deleteBst(root, deleteValue);
-            cout << "Value deleted successfully. Press 1 to display traversals.\n";
+            int valu;
+            cin >> valu;
+            deleteb(root, valu);
+            cout << "deleted successfully\n";
             break;
         case 4:
-            cout << "Exiting program...\n";
+            system("cls");
+            display(root);
             break;
+        case 5:
+            cout << "exit\n";
+            return 0;
         default:
-            cout << "Invalid choice, please try again.\n";
+            cout << "invalid choice\n";
         }
-        system("pause");
-    } while (choice != 4);
-
+    }
     return 0;
 }
